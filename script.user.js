887e76d0a9c0e5d5293af824f175a05c6ec39016
Puls3 <puls3@puls3.dev>
2024-04-26 17:07:06 -0400

    Added declarations for automatic updates

--- script.user.js
+++ script.user.js
@@ -1,8 +1,12 @@
 // ==UserScript==
 // @name New Reddit Redirect
-// @version      1.0.1
-// @description  redirects you to new.reddit.com
-// @author       github.com/xPuls3
+// @version 1.0.1
+// @description redirects you to new.reddit.com
+// @author github.com/xPuls3
+// @homepage https://github.com/xPuls3/New-Reddit-Redirect
+// @supportURL https://github.com/xPuls3/New-Reddit-Redirect/issues
+// @downloadURL https://github.com/xPuls3/New-Reddit-Redirect/raw/master/script.user.js
+// @updateURL https://github.com/xPuls3/New-Reddit-Redirect/raw/master/script.user.js
 // @match *://*.reddit.com/*
 // @exclude /^https?://[a-z]{2}\.reddit\.com/*
 // @exclude *out.reddit.com/*
@@ -11,10 +15,14 @@
 // @exclude *chat.reddit.com/*
 // @exclude *://*.reddit.com/gallery/*
 // @exclude *://*.reddit.com/media*
-// @exclude  *://*.reddit.com/mod/*/insights
+// @exclude *://*.reddit.com/mod/*/insights
 // @run-at document-start
-// @grant        none
+// @grant none
 // ==/UserScript==
+
+// This script was created by Puls3!
+// - https://github.com/xPuls3
+// - https://gitlab.com/Puls3
 
 // Forked from RichKMLS's Old Reddit Redirect script
 // - https://github.com/RichKMLS/Old-Reddit-Redirect
