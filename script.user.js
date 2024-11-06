9df42f18c10bcb78dd81141eb33342e4faec0b1f
Noah Kesterson <35641648+NoahBK@users.noreply.github.com>
2024-11-06 10:44:17 -0500

    Update script.user.js
    Updated the version number from 1.1 to 1.2 to reflect changes
    
    Replaced previous user urls with mine to reflect new fork
    
    Added an exception for /achievements

--- script.user.js
+++ script.user.js
@@ -1,34 +1,31 @@
 // ==UserScript==
-// @name New Reddit Redirect
-// @version 1.1
-// @description Redirects you to new.reddit.com
-// @author github.com/xPuls3
-// @homepage https://github.com/xPuls3/New-Reddit-Redirect
-// @supportURL https://github.com/xPuls3/New-Reddit-Redirect/issues
-// @downloadURL https://github.com/xPuls3/New-Reddit-Redirect/raw/master/script.user.js
-// @updateURL https://github.com/xPuls3/New-Reddit-Redirect/raw/master/script.user.js
-// @match *://*.reddit.com/*
-// @exclude /^https?://[a-z]{2}\.reddit\.com/*
-// @exclude *out.reddit.com/*
-// @exclude *mod.reddit.com/*
-// @exclude *old.reddit.com/*
-// @exclude *chat.reddit.com/*
-// @exclude *://*.reddit.com/gallery/*
-// @exclude *://*.reddit.com/media*
-// @exclude *://*.reddit.com/mod/*/insights
-// @run-at document-start
-// @grant none
+// @name         New Reddit Redirect
+// @version      1.2
+// @description  Redirects you to new.reddit.com
+// @author       edit by https://github.com/NoahBK
+// @homepage     https://github.com/NoahBK
+// @supportURL   https://github.com/NoahBK/New-Reddit-Redirect/issues
+// @downloadURL  https://github.com/NoahBK/New-Reddit-Redirect/raw/master/script.user.js
+// @updateURL    https://github.com/NoahBK/New-Reddit-Redirect/raw/master/script.user.js
+// @match        *://*.reddit.com/*
+// @exclude      /^https?://[a-z]{2}\.reddit\.com/*
+// @exclude      *out.reddit.com/*
+// @exclude      *mod.reddit.com/*
+// @exclude      *old.reddit.com/*
+// @exclude      *chat.reddit.com/*
+// @exclude      *://*.reddit.com/gallery/*
+// @exclude      *://*.reddit.com/media*
+// @exclude      *://*.reddit.com/mod/*/insights
+// @run-at       document-start
+// @grant        none
 // ==/UserScript==
-
-// This script was created by Puls3!
-// - https://github.com/xPuls3
-// - https://gitlab.com/Puls3
 
 'use strict';
 
 const currentHostname = window.location.hostname;
 const targetHostname = "new.reddit.com";
 
-if (!currentHostname.includes("new.reddit.com")) {
+// Redirect to new Reddit if not already there and if the URL is not excluded
+if (!currentHostname.includes("new.reddit.com") && !window.location.href.includes("/achievements")) {
     window.location.hostname = targetHostname;
 }
